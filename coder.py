30a7f9e08951905cd38abbabfebd53ee0d7698c7
MrSlime <slime.kostia@gmail.com>
2025-09-04 00:13:37 +0300

    start doing encoder

--- coder.py
+++ coder.py
@@ -1,19 +1,27 @@
-from PIL import Image
-import numpy as np # type: ignore
-
 class encoder():
     
-    img = None
+    def coords_to_colors(self, data, pix):
+        colors = []
+        for x in data:
+            plus=next(pix)
+            colors.append((x[0]+plus[0]-3, x[1]+plus[1]-3, x[2]+plus[2]-3))
+        return colors
 
-    def open_image(self, path):
-        img = Image.open(path)
-        img.convert('RGB')
-        self.img = img
-        return img 
-
-    def image_to_data(self, img):
-        app = np.array(img)
-        return app
+    def encode(self, data, height, width, pix):
+        colors = self.coords_to_colors(data, pix)
+        len_colors = len(colors)
+        res = []
+        for i in range(height):
+            for j in range(width):
+                basik = next(pix)
+                res.append((basik[0],basik[1],basik[2]))
+                for _ in range(3):
+                    if len_colors > 0:
+                        res.append(next(colors))
+                        len_colors -= 1
+                    else:
+                        res.append((basik[0],basik[1],basik[2]))
+        return res
 
         
 class decoder():
--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import matrix
+import coder
+import numpy as np # type: ignore
+from PIL import Image
 
-while True:
-    i = input()
-    if i == "p":
-        target = (int(input()), int(input()), int(input()))
-        print(matrix.post_coords(target))
-    elif i == "g":
-        target = str(input())
-        print(matrix.get_coords(target))
+#while True:
+#    i = input()
+#    if i == "p":
+#        target = (int(input()), int(input()), int(input()))
+#        print(matrix.post_coords(target))
+#    elif i == "g":
+#        target = str(input())
+#        print(matrix.get_coords(target))
+
+def open_image(path):
+        img = Image.open(path)
+        img.convert('RGB')
+        return img 
+
+def image_to_data(img):
+    pix = np.array(img)
+    return pix.reshape(-1, 3)
+
+def encode(text, path):
+    data = []
+    for char in text:
+         data.append(matrix.get_coords(char))
+    img = open_image(path)
+    pix = image_to_data(img)
+    height, width = img.size    
+    colors = coder.encoder.encode(data, height, width, pix)
+    new_img = Image.new('RGB', (width*2, height*2))
+    pixels = new_img.load()
+    for y in range(height):
+        for x in range(width):
+            pixels[x*2, y*2] = next(colors)
+            pixels[x*2+1, y*2] = next(colors)
+            pixels[x*2, y*2+1] = next(colors)
+            pixels[x*2+1, y*2+1] = next(colors)
+    new_img.save('encoded.png')
+
+encode(str(input()), str(input()))
