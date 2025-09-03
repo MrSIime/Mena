869a98df330f5b7c00b021254409126acfe08c79
MrSlime <slime.kostia@gmail.com>
2025-09-03 21:15:32 +0300

    added 2 defs

--- coder.py
+++ coder.py
@@ -1,7 +1,20 @@
+from PIL import Image
+import numpy as np # type: ignore
+
 class encoder():
-    pass
+    
+    img = None
 
+    def open_image(self, path):
+        img = Image.open(path)
+        img.convert('RGB')
+        self.img = img
+        return img 
 
+    def image_to_data(self, img):
+        app = np.array(img)
+        return app
 
+        
 class decoder():
     pass
